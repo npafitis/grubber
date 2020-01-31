2d94614537fab48aee16399c3269a72d8d34080d
Nikolas Pafitis <nikolas.p@trgint.com>
2020-01-31 15:27:51 +0200

    Creating graph structure.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.zeromq/jeromq "0.5.1"]
+                 [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]]
   :profiles {:grubber    {:main grubber.core}
              :grubmaster {:main grubmaster.core}}
   :repl-options {:main grubber.core})
--- src/grubmaster/core.clj
+++ src/grubmaster/core.clj
@@ -1,4 +1,54 @@
-(ns grubmaster.core)
+(ns grubmaster.core
+  (:import (java.util List)))
+;;;;;;;;;;;;;;;;;
+;; Graph Record
+;;;;;;;;;;;;;;;;;
 
-(defn bar []
-  (prn "World Hello"))
+(defrecord Graph [^List nodes
+                  ^List links])
+
+;;;;;;;;;;;;;;;;;
+;; Node Records
+;;;;;;;;;;;;;;;;;
+
+(defprotocol Node
+  (node-id [this])
+  (node-fn [this]))
+
+(defrecord MapNode [id fn]
+  Node
+  (node-id [this] (:id this))
+  (node-fn [this] (:fn this)))
+
+(defrecord ReduceNode [id fn]
+  Node
+  (node-id [this] (:id this))
+  (node-fn [this] (:fn this)))
+
+;;;;;;;;;;;;;;;;;
+;; Link Record
+;;;;;;;;;;;;;;;;;
+
+(defrecord Link [in out type])
+
+;;;;;;;;;;;;;;;;;
+;; Actions
+;;;;;;;;;;;;;;;;;
+
+(defn node-id-exists? [^Graph graph id]
+  (not (empty? (filter #(= (node-id %) id) (:nodes graph)))))
+
+(defn node-exists? [^Graph graph
+                    node]
+  (node-id-exists? graph (:id node)))
+
+(defn add-node [^Graph graph
+                node]
+  (if (node-exists? graph node)
+    graph
+    (update-in graph [:nodes] conj node)))
+
+
+(def graph (-> (->Graph nil nil)
+               (add-node (->MapNode 1 nil))
+               (add-node (->MapNode 2 nil))))
