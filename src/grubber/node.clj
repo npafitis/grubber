4a0b0a221333011146d6e075bd3d6fc7760ca8f9
Nikolas Pafitis <nikolas.p@trgint.com>
2020-02-07 10:14:38 +0200

    Code cleanup.

--- src/grubber/node.clj
+++ src/grubber/node.clj
@@ -14,17 +14,20 @@
 (defn get-node [port]
   (:node (@node-context-map port)))
 
+(defn get-acc [port]
+  (:acc (@node-context-map port)))
+
 (defn get-node-properties [port]
   (:node-properties (@node-context-map port)))
+
+(defn get-node-emit-sock [port]
+  (:emit-sock (get-node-properties port)))
 
 (defn get-threads [port]
   (:threads (get-node port)))
 
 (defn get-node-fn [port]
   (:fn (get-node port)))
-
-(defn get-acc [port]
-  (:acc (@node-context-map port)))
 
 (defn get-runner [port emitter]
   (let [node-properties (get-node-properties port)
@@ -60,6 +63,10 @@
                        (fn [node-context]
                          (update-in node-context [port :acc] #((eval reducer) % data)))))))
 
+(defn ndeal [_ port emitter]
+  (fn [data]
+    nil))
+
 (defn emitter-connect! [emitter port]
   (let [outs (:out (get-node port))
         out (first outs)]
@@ -68,23 +75,24 @@
           (log/info "Emitter connecting to " out)
           (zmq/connect emitter (str "tcp://" out))))))
 
-(defn single-pipeline! [context emit-sock input-chan port]
+(defn single-pipeline! [context input-chan port]
   (log/info "Starting single-threaded pipeline...")
   (async/go
-    (with-open [emitter (zmq/socket context emit-sock)]
-      (emitter-connect! emitter port)
-      (loop [data (async/<! input-chan)]
-        (let [runner (get-runner port emitter)]
-          (runner data))
-        (or (end-of-stream? data)
-            (recur (async/<! input-chan)))))))
+    (let [emit-sock (get-node-emit-sock port)]
+      (with-open [emitter (zmq/socket context emit-sock)]
+        (emitter-connect! emitter port)
+        (loop [data (async/<! input-chan)]
+          (let [runner (get-runner port emitter)]
+            (runner data))
+          (or (end-of-stream? data)
+              (recur (async/<! input-chan))))))))
 
-(defn threaded-pipeline! [context emit-sock input-chan port]
+(defn threaded-pipeline! [context input-chan port threads]
   (log/info "Starting threaded pipeline...")
-  (doseq [_ (range 0 (get-threads port))]
-    (single-pipeline! context emit-sock input-chan port)))
+  (doseq [_ (range 0 threads)]
+    (single-pipeline! context input-chan port)))
 
-(defn run-node! [context emit-sock consume-sock port]
+(defn run-node! [context consume-sock port]
   (log/info "Running node at port: " port)
   (async/go
     (with-open [consumer (doto (zmq/socket context consume-sock)
@@ -92,19 +100,17 @@
 
       (let [input-chan (async/chan 1)
             threads (get-threads port)]
-        (if (> threads 1)
-          (threaded-pipeline! context emit-sock input-chan port)
-          (single-pipeline! context emit-sock input-chan port))
+
+        (threaded-pipeline! context input-chan port threads)
 
         (loop [data (utils/read-sock consumer)]
           (log/info "Read " data "from consumer socket")
           (if (end-of-stream? data)
-            ;; If end of stream then broadcast to all workers
-            (do
-              (log/info "End of stream received")
-              (doseq [_ (range 0 threads)]
-                (log/info "Passing :end-of-stream to input channel")
-                (async/>! input-chan :end-of-stream)))
+            ;; TODO: Doesn't work properly.( Race Conditions)
+
+            (doseq [_ (range 0 threads)]
+              (log/info "Passing :end-of-stream to input channel")
+              (async/>! input-chan :end-of-stream))
 
             (do
               (log/info "Pushing data to input channel: (Data " data ")")
@@ -117,6 +123,9 @@
                                :consume-sock :pull}
                       :reduce {:runner       #'nreduce
                                :emit-sock    :push
+                               :consume-sock :pull}
+                      :dealer {:runner       #'ndeal
+                               :emit-sock    :dealer
                                :consume-sock :pull}})
 
 (defn run-grubber! [node context]
@@ -126,5 +135,5 @@
         consume-sock (:consume-sock properties)
         port (get-available-port)]
     (update-node-context port (->NodeCtx node properties nil nil))
-    (run-node! @context emit-sock consume-sock port)
+    (run-node! @context consume-sock port)
     port))
