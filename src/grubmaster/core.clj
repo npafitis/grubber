918dbd72655a65fd0edb8f9de526ef4eb12528a6
Nikolas Pafitis <nikolas.p@trgint.com>
2020-02-07 09:03:18 +0200

    Code style improvements.

--- src/grubmaster/core.clj
+++ src/grubmaster/core.clj
@@ -2,22 +2,13 @@
   (:require [grubmaster.graph :refer :all]
             [grubmaster.node :refer :all]))
 
-;(def graph (-> (create-graph)
-;               (add-node (create-map-node
-;                           {:id 1 :transformer '(fn [x] (inc x)) :url "localhost" :port 8080}))
-;               (add-node (create-map-node
-;                           {:id 2 :transformer '(fn [x] (* x x)) :url "localhost" :port 8080}))
-;               (add-link {:src :vent :dst 1})
-;               (add-link {:src 1 :dst 2})
-;               (add-link {:src 2 :dst :sink})))
-
 (def graph (-> (create-graph)
                (add-node (create-map-node
-                           {:id 1 :transformer '(fn [x] (inc x)) :url "localhost" :port 8080}))
+                           {:id 1 :transformer '(fn [x] (inc x))}))
                (add-node (create-map-node
-                           {:id 2 :transformer '(fn [x] (* x x)) :url "localhost" :port 8080}))
+                           {:id 2 :transformer '(fn [x] (* x x))}))
                (add-node (create-reduce-node
-                           {:id 3 :reducer '(fn [acc, x] (+ (or acc 0) x)) :url "localhost" :port 8080}))
+                           {:id 3 :reducer '(fn [acc, x] (+ (or acc 0) x))}))
                (add-link {:src :vent :dst 1})
                (add-link {:src 1 :dst 2})
                (add-link {:src 2 :dst 3})
--- src/grubmaster/node.clj
+++ src/grubmaster/node.clj
@@ -8,11 +8,13 @@
 (defrecord Node [id fn url port out type threads])
 
 
-(defn create-map-node [{:keys [id transformer url port threads]}]
-  (->Node id transformer (or url "localhost") (or port "8080") [] :map (or threads 1)))
+(defn create-map-node [{:keys [id transformer url port threads]
+                        :or   {url "localhost" port 8080 threads 1}}]
+  (->Node id transformer url port [] :map threads))
 
-(defn create-reduce-node [{:keys [id reducer url port threads]}]
-  (->Node id reducer (or url "localhost") (or port "8080") [] :reduce (or threads 1)))
+(defn create-reduce-node [{:keys [id reducer url port threads]
+                           :or   {url "localhost" port 8080 threads 1}}]
+  (->Node id reducer url port [] :reduce threads))
 
 (defn add-node-relation [^Node node
                          port
